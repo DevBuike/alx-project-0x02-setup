897a9686bde6214da1aa4e5c8e00dfb4c578c6c8
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-07 17:31:56 +0000

    adding basic contents

---
+++ alx-project-0x02/interfaces/index.tsx
@@ -0,0 +1,13 @@
+interface Custom {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    price: number;
+    category: string;
+    stock: number;
+    rating: number;
+    reviews: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
