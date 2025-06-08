30cc128034cab39f458557efd9844809583a81d1
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-08 06:53:31 +0000

    updated the card.tsx to include { type...}

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,13 +1,16 @@
 import Header from '@/components/layout/Header';
 import Card from '@/components/common/Card';
+import {useMemo} from 'react';
 
 function Home() {
+  const cardId = useMemo(() => crypto.randomUUID(), []);
+
   return (
     <>
       <Header />
       <div>
         <Card
-          id= {crypto.randomUUID()}
+          id= {cardId}
           title="Welcome to My Application"
           content="This is a simple card component that displays a title and some content."
         />
