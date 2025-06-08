18619985b8eb3f729baccec5c5341e81ccf02dca
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-08 21:18:57 +0000

    update

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -34,4 +34,16 @@ function Posts() {
   );
 }
 
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Posts;
